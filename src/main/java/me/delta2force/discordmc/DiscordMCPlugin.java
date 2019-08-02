17e8d6e264ee4392435bceb644c876edb45668e7
Delta2Force <lou.gr@outlook.com>
2019-08-02 10:39:10 +0200

    OnCommand() beginnings

--- src/main/java/me/delta2force/discordmc/DiscordMCPlugin.java
+++ src/main/java/me/delta2force/discordmc/DiscordMCPlugin.java
@@ -1,12 +1,31 @@
 package me.delta2force.discordmc;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.delta2force.discordmc.listener.EventListener;
+import net.md_5.bungee.api.ChatColor;
 
 public class DiscordMCPlugin extends JavaPlugin{
 	@Override
 	public void onEnable() {
 		this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(sender instanceof Player) {
+			Player p = (Player) sender;
+			if(!p.isOp()) {
+				p.sendMessage(ChatColor.RED + "This can only be executed as an OP!");
+			}else {
+				
+			}
+		}else {
+			sender.sendMessage(ChatColor.RED + "This can only be executed as a player!");
+		}
+		return true;
 	}
 }
