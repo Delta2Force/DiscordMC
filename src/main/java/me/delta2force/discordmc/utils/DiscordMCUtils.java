e0786e3ffaccba7b9a173b569c4498dcbf8de83b
Delta2Force <lou.gr@outlook.com>
2019-08-02 11:04:53 +0200

    add JDAEventListener

--- src/main/java/me/delta2force/discordmc/utils/DiscordMCUtils.java
+++ src/main/java/me/delta2force/discordmc/utils/DiscordMCUtils.java
@@ -8,8 +8,10 @@ import org.bukkit.ChatColor;
 import me.delta2force.discordmc.DiscordMCPlugin;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.hooks.EventListener;
 
-public class DiscordMCUtils {
+public class DiscordMCUtils implements EventListener{
 	private DiscordMCPlugin discordMC;
 	private JDA jdaClient;
 	
@@ -17,14 +19,23 @@ public class DiscordMCUtils {
 		this.discordMC = discordMC;
 		try {
 			this.jdaClient = new JDABuilder(discordMC.getConfig().getString("botToken")).build();
+			this.jdaClient.addEventListener(this);
 		} catch (LoginException e) {
-			Bukkit.broadcastMessage(ChatColor.BLUE + "[DiscordMC] " + ChatColor.RED + "There was an error while logging in using DiscordMC! Tell the admins to look in the console! I'm outta here.");
+			Bukkit.broadcastMessage(getPrefix() + ChatColor.RED + "There was an error while logging in using DiscordMC! Tell the admins to look in the console! I'm outta here.");
 			Bukkit.getServer().getPluginManager().disablePlugin(discordMC);
 			e.printStackTrace();
 		}
 	}
 	
+	public String getPrefix() {
+		return ChatColor.BLUE + "[DiscordMC] ";
+	}
+	
 	public JDA getClient() {
 		return jdaClient;
+	}
+
+	@Override
+	public void onEvent(Event event) {
 	}
 }
